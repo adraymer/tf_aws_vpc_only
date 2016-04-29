59b3d4a74354a6346e742d3230b9f68fb8ef313d
Adam <a@ge.com>
2016-04-28 23:01:09 -0400

    adding dedicated VPC tenancy

--- main.tf
+++ main.tf
@@ -11,11 +11,16 @@ variable "enable_dns_hostnames" {
   description = "should be true if you want to use private DNS within the VPC"
   default = false
 }
+variable "enable_instance_tenancy" {
+  description = "should be dedicated if you want to use private instances"
+  default = "dedicated"
+}
 
 resource "aws_vpc" "vpc" {
   cidr_block           = "${var.cidr}"
   enable_dns_support   = "${var.enable_dns_support}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
+  instance_tenancy = "${var.enable_instance_tenancy}"
 
   tags {
     Name = "${var.name}"
